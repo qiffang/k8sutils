efef1899ca940df8373aa5648b8db4ec4a2a4337
qiffang <947321353@qq.com>
2023-03-03 14:34:26 +0800

    exec cli

---
+++ pkg/exec/client.go
@@ -0,0 +1,39 @@
+package exec
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"time"
+)
+
+// Clientset knows how to interact with a K8s cluster. It has a reference to
+// user configuration stored in viper.
+
+type Client struct {
+	kubernetes.Interface
+	*ClientOpt
+}
+
+type ClientOpt struct {
+	K8sConfig *rest.Config
+
+	PodName       string
+	ContainerName string
+	Namespace     string
+
+	CurrentContext string
+	RequestTimeout time.Duration
+}
+
+// NewClient returns a new Clientset for the given config.
+func NewClient(opt *ClientOpt) (*Client, error) {
+	k8sClientset, err := kubernetes.NewForConfig(opt.K8sConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		ClientOpt: opt,
+		Interface: k8sClientset,
+	}, nil
+}
---
+++ pkg/exec/exec.go
@@ -0,0 +1,133 @@
+package exec
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/martian/log"
+	"io"
+	"k8s.io/apimachinery/pkg/util/httpstream/spdy"
+	spdy2 "k8s.io/client-go/transport/spdy"
+	"net/http"
+	"net/url"
+	"strings"
+
+	authzv1 "k8s.io/api/authorization/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+)
+
+var deniedCreateExecErr = fmt.Errorf("no permissions to create exec subresource")
+
+// ExecPod issues an exec request to execute the given command to a particular
+// pod.
+func (c *Client) ExecPod(command []string, stdin io.Reader, stdout, stderr io.Writer, tty bool) error {
+	log.Infof("sending exec request, command=%s, namespace=%S, pod=%s, container=%s", strings.Join(command, " "))
+
+	execRequest := c.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Namespace(c.Namespace).
+		Name(c.PodName).
+		SubResource("exec").
+		Timeout(c.RequestTimeout)
+
+	execRequest = execRequest.VersionedParams(&corev1.PodExecOptions{
+		Container: c.ContainerName,
+		Command:   command,
+		Stdin:     stdin != nil,
+		Stdout:    stdout != nil,
+		Stderr:    stderr != nil,
+		TTY:       tty,
+	}, scheme.ParameterCodec)
+
+	exec, err := newExecutor(c.K8sConfig, "POST", execRequest.URL())
+	if err != nil {
+		return fmt.Errorf("failed to set up executor: %w", err)
+	}
+
+	if err := exec.Stream(remotecommand.StreamOptions{
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+		Tty:    tty,
+	}); err != nil {
+		return fmt.Errorf("failed to exec command: %w", err)
+	}
+
+	return nil
+}
+
+// CanExec determines if the current user can create a exec subresource in the
+// given pod.
+func (c *Client) CanExec() error {
+	selfAccessReview := &authzv1.SelfSubjectAccessReview{
+		Spec: authzv1.SelfSubjectAccessReviewSpec{
+			ResourceAttributes: &authzv1.ResourceAttributes{
+				Namespace:   c.Namespace,
+				Verb:        "create",
+				Group:       "",
+				Resource:    "pods",
+				Subresource: "exec",
+				Name:        "",
+			},
+		},
+	}
+
+	log.Infof("checking for exec permissions. namespace=%s, request-timeout=%s", c.Namespace, c.RequestTimeout.String())
+
+	ctx, cancel := context.WithTimeout(context.Background(), c.RequestTimeout)
+	defer cancel()
+
+	response, err := c.AuthorizationV1().SelfSubjectAccessReviews().Create(ctx, selfAccessReview, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	if !response.Status.Allowed {
+		if response.Status.Reason != "" {
+			return fmt.Errorf("%w. reason: %s", deniedCreateExecErr, response.Status.Reason)
+		}
+		return deniedCreateExecErr
+	}
+
+	log.Infof("confirmed exec permissions. namespace=%s, request-timeout=%s", c.Namespace, c.RequestTimeout.String())
+	return nil
+}
+
+var newExecutor = func(config *rest.Config, method string, url *url.URL) (remotecommand.Executor, error) {
+	return NewSPDYExecutor(config, method, url)
+}
+
+func NewSPDYExecutor(config *restclient.Config, method string, url *url.URL) (remotecommand.Executor, error) {
+	wrapper, upgradeRoundTripper, err := RoundTripperFor(config)
+	if err != nil {
+		return nil, err
+	}
+	return remotecommand.NewSPDYExecutorForTransports(wrapper, upgradeRoundTripper, method, url)
+}
+
+func RoundTripperFor(config *restclient.Config) (http.RoundTripper, spdy2.Upgrader, error) {
+	tlsConfig, err := restclient.TLSConfigFor(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	proxy := http.ProxyFromEnvironment
+	if config.Proxy != nil {
+		proxy = config.Proxy
+	}
+	upgradeRoundTripper := spdy.NewRoundTripperWithConfig(spdy.RoundTripperConfig{
+		TLS:                      tlsConfig,
+		FollowRedirects:          true,
+		RequireSameHostRedirects: false,
+		Proxier:                  proxy,
+		PingPeriod:               0,
+	})
+	wrapper, err := restclient.HTTPWrappersForConfig(config, upgradeRoundTripper)
+	if err != nil {
+		return nil, nil, err
+	}
+	return wrapper, upgradeRoundTripper, nil
+}
